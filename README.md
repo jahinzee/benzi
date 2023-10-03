1b2ea0baf9e5b28d502ffe2182215ffe58390678
jahinzee <jahinzee@outlook.com>
2023-10-03 18:05:12 +1100

    added sound support

--- README.md
+++ README.md
@@ -12,6 +12,17 @@ pip install -r requirements.txt
 
 # Usage
 
+```
+usage: benzi.py [-h] [-s SOUND]
+
+big ben on your desktop.
+
+options:
+  -h, --help            show this help message and exit
+  -s SOUND, --sound SOUND
+                        specify a sound file to play on the hour
+```
+
 The script runs synchronously, meaning you will need to run it in the background if you don't want a Python console window sitting around. On Windows, you can use `pythonw`, and on Linux, you may set up a custom daemon, or alternatively the following command will work:
 
 ```sh
--- benzi.py
+++ benzi.py
@@ -12,18 +12,48 @@
 from desktop_notifier import DesktopNotifier
 import schedule
 import time
+from playsound import playsound
+from os import path
 from datetime import datetime
+import argparse
 
 
-def notify(notifier):
+def notify(notifier, sound):
+    if sound is not None:
+        playsound(sound, 0)
     notifier.send_sync(title=f"It is {datetime.now():%H:%M}.",
                        message="",
                        icon=(""))
 
 
+def notify_sound_error(notifier, sound_path):
+    notifier.send_sync(title=f"File '{sound_path}' not found.",
+                       message="Notifications will play silently.",
+                       icon=(""))
+
+
+def get_sound(notifier, sound_path):
+    if sound_path is None:
+        return None
+    if path.exists(sound_path):
+        return sound_path
+    else:
+        notify_sound_error(notifier, sound_path)
+        return None
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="big ben on your desktop.")
+    parser.add_argument('-s', '--sound', type=str,
+                        help="specify a sound file to play on the hour")
+    return parser.parse_args()
+
+
 def main():
+    args = get_args()
     notifier = DesktopNotifier("benzi")
-    schedule.every().hour.at(":00").do(lambda: notify(notifier))
+    sound = get_sound(notifier, args.sound)
+    schedule.every().hour.at(":00").do(lambda: notify(notifier, sound))
     while True:
         schedule.run_pending()
         time.sleep(.1)
